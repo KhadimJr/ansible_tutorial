00c7670c42ebf2256a10154613a473b2eb4d5596
ebrima <ejaw@utg.edu.gm>
2020-12-22 10:21:23 +0800

    new commit for code

--- review/main.py
+++ review/main.py
@@ -1,1 +1,14 @@
 
+a = "Networking is fun, but "
+b = 'DevOps is fun too. However, '
+c = """What about Coding?....Super fun"""
+print(" ")
+print(a + b + c)
+
+def tesing(num, num1):
+    """
+    docstring
+    """
+    results = num + num1
+    return results
+print(tesing(10, 40))
